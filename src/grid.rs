69cd25a370e0111c40bd7b42d190cc1d42da25d9
opusbopus <opusbopus@proton.me>
2024-04-23 21:59:21 -0400

    Move grid structures to own module, Add PixelKind, Sand color, Display currently selected PixelKind (only sand for now)

---
+++ src/grid.rs
@@ -0,0 +1,82 @@
+use raylib::color::Color;
+
+#[derive(Debug, Clone, Copy)]
+pub enum PixelKind {
+    Sand,
+}
+
+impl PixelKind {
+    pub const fn color(&self) -> Color {
+        Color {
+            r: 194,
+            g: 178,
+            b: 128,
+            a: 255,
+        }
+    }
+
+    pub const fn name(&self) -> &'static str {
+        match self {
+            PixelKind::Sand => "Sand",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PixelState {
+    pub enabled: bool,
+    pub kind: PixelKind,
+}
+
+impl Default for PixelState {
+    fn default() -> Self {
+        Self {
+            enabled: false,
+            kind: PixelKind::Sand,
+        }
+    }
+}
+
+pub struct PixelGrid {
+    states: Vec<Vec<PixelState>>,
+}
+
+impl PixelGrid {
+    pub fn new(columns: usize, rows: usize) -> Self {
+        let mut states: Vec<Vec<PixelState>> = Vec::with_capacity(columns);
+        for i in 0..columns {
+            let mut rows = Vec::with_capacity(rows);
+            for j in 0..rows.capacity() {
+                rows.insert(j, PixelState::default());
+            }
+            states.insert(i, rows);
+        }
+        Self { states }
+    }
+
+    pub fn get_state(&self, col: i32, row: i32) -> Option<&PixelState> {
+        if col.is_negative() || row.is_negative() {
+            return None;
+        }
+
+        self.states
+            .get(col as usize)
+            .map(|rows| rows.get(row as usize))?
+    }
+
+    pub fn get_state_mut(&mut self, col: i32, row: i32) -> Option<&mut PixelState> {
+        if col.is_negative() || row.is_negative() {
+            return None;
+        }
+
+        self.states
+            .get_mut(col as usize)
+            .map(|rows| rows.get_mut(row as usize))?
+    }
+
+    pub fn update_state<F: Fn(&mut PixelState)>(&mut self, col: i32, row: i32, transform: F) {
+        if let Some(state) = self.get_state_mut(col, row) {
+            transform(state);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,8 @@
+mod grid;
+
+use grid::{PixelGrid, PixelKind, PixelState};
 use rand::{rngs::ThreadRng, Rng};
 use raylib::prelude::*;
-
-#[derive(Debug, Clone, Copy)]
-struct PixelState {
-    pub enabled: bool,
-}
-
-impl PixelState {
-    pub fn toggle(&mut self) {
-        self.enabled = !self.enabled;
-    }
-}
-
-impl Default for PixelState {
-    fn default() -> Self {
-        Self { enabled: false }
-    }
-}
-
-struct PixelGrid {
-    states: Vec<Vec<PixelState>>,
-}
-
-impl PixelGrid {
-    pub fn new(columns: usize, rows: usize) -> Self {
-        let mut states: Vec<Vec<PixelState>> = Vec::with_capacity(columns);
-        for i in 0..columns {
-            let mut rows = Vec::with_capacity(rows);
-            for j in 0..rows.capacity() {
-                rows.insert(j, PixelState::default());
-            }
-            states.insert(i, rows);
-        }
-        Self { states }
-    }
-
-    pub fn get_state(&self, col: i32, row: i32) -> Option<&PixelState> {
-        if col.is_negative() || row.is_negative() {
-            return None;
-        }
-
-        self.states
-            .get(col as usize)
-            .map(|rows| rows.get(row as usize))?
-    }
-
-    pub fn get_state_mut(&mut self, col: i32, row: i32) -> Option<&mut PixelState> {
-        if col.is_negative() || row.is_negative() {
-            return None;
-        }
-
-        self.states
-            .get_mut(col as usize)
-            .map(|rows| rows.get_mut(row as usize))?
-    }
-}
 
 const PIXEL_SIZE: i32 = 5;
 const WIDTH: i32 = 1200;
@@ -72,6 +20,7 @@ fn main() {
 
     let mut pixel_grid = PixelGrid::new(COLUMNS as usize, ROWS as usize);
     let mut rng = ThreadRng::default();
+    let selected_pixel_kind = PixelKind::Sand;
     while !handle.window_should_close() {
         let lmb_held = handle.is_mouse_button_down(MouseButton::MOUSE_LEFT_BUTTON);
         let rmb_held = handle.is_mouse_button_down(MouseButton::MOUSE_RIGHT_BUTTON);
@@ -80,39 +29,37 @@ fn main() {
 
         let mut draw = handle.begin_drawing(&thread);
         draw.clear_background(Color::BLACK);
+        draw.draw_text(
+            &format!("Currently dropping: {}", selected_pixel_kind.name()),
+            4,
+            4,
+            20,
+            Color::RAYWHITE,
+        );
 
         if lmb_held {
             let col = (mouse_pos.x / PIXEL_SIZE as f32).floor() as i32;
             let row = (mouse_pos.y / PIXEL_SIZE as f32).floor() as i32;
             if let Some(_) = pixel_grid.get_state(col, row) {
+                let transform = |state: &mut PixelState| {
+                    state.enabled = true;
+                    state.kind = selected_pixel_kind;
+                };
+
                 for i in -3..3 {
-                    let state = if rng.gen_bool(0.5) {
-                        pixel_grid.get_state_mut(col + i, row)
+                    if rng.gen_bool(0.5) {
+                        pixel_grid.update_state(col + i, row, transform);
                     } else {
-                        pixel_grid.get_state_mut(col, row + i)
-                    };
-
-                    if let Some(state) = state {
-                        if !state.enabled {
-                            state.toggle();
-                        }
+                        pixel_grid.update_state(col, row + i, transform);
                     }
                 }
 
-                if let Some(state) = pixel_grid.get_state_mut(col, row) {
-                    if !state.enabled {
-                        state.toggle();
-                    }
-                }
+                pixel_grid.update_state(col, row, transform);
             }
         } else if rmb_held {
             let col = (mouse_pos.x / PIXEL_SIZE as f32).floor() as i32;
             let row = (mouse_pos.y / PIXEL_SIZE as f32).floor() as i32;
-            if let Some(state) = pixel_grid.get_state_mut(col, row) {
-                if state.enabled {
-                    state.toggle();
-                }
-            }
+            pixel_grid.update_state(col, row, |state| state.enabled = false);
         }
 
         // draw pixels
@@ -125,7 +72,7 @@ fn main() {
                 if state.enabled {
                     let x = col * PIXEL_SIZE;
                     let y = row * PIXEL_SIZE;
-                    draw.draw_rectangle(x, y, PIXEL_SIZE, PIXEL_SIZE, Color::WHITE);
+                    draw.draw_rectangle(x, y, PIXEL_SIZE, PIXEL_SIZE, state.kind.color());
                 }
             }
         }
@@ -153,33 +100,23 @@ fn main() {
                             }
 
                             if left_empty && right_empty {
-                                if let Some(state) =
-                                    new_grid.get_state_mut(col + direction, row + 1)
-                                {
-                                    state.enabled = true;
-                                }
+                                new_grid.update_state(col + direction, row + 1, |state| {
+                                    state.enabled = true
+                                });
                             } else if left_empty {
-                                if let Some(state) = new_grid.get_state_mut(col - 1, row + 1) {
-                                    state.enabled = true;
-                                }
+                                new_grid
+                                    .update_state(col - 1, row + 1, |state| state.enabled = true);
                             } else if right_empty {
-                                if let Some(state) = new_grid.get_state_mut(col + 1, row + 1) {
-                                    state.enabled = true;
-                                }
+                                new_grid
+                                    .update_state(col + 1, row + 1, |state| state.enabled = true);
                             } else {
-                                if let Some(state) = new_grid.get_state_mut(col, row) {
-                                    state.enabled = true;
-                                }
+                                new_grid.update_state(col, row, |state| state.enabled = true);
                             }
                         } else {
-                            if let Some(state) = new_grid.get_state_mut(col, row + 1) {
-                                state.enabled = true;
-                            }
+                            new_grid.update_state(col, row + 1, |state| state.enabled = true);
                         }
                     } else {
-                        if let Some(state) = new_grid.get_state_mut(col, row) {
-                            state.enabled = true;
-                        }
+                        new_grid.update_state(col, row, |state| state.enabled = true);
                     }
                 }
             }
